212250c0de9771de7238bfedb7f6e1ff6693c47d
Jasss <153324785+lakapaka600@users.noreply.github.com>
2024-09-22 16:50:27 +0200

    jass 2

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int l,b,a;
+    printf("lato");
+    scanf("%d",&l);
+    printf("base");
+    scanf("%d",&b);
+    a=l*b;
+    printf("%d",a);
+    return 0;
+}
